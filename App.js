2d4a08bcd903c70ddb762e8056be9babb51ac9a0
Sudip Thapa <sudeptech@gmail.com>
2021-03-01 16:26:19 +0545

    ubuntu font added

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading'
 import AppNavigator from './navigations/AppNavigator';
 
+
 export default function App() {
-  return (
-    <AppNavigator />
-  );
+  const [isReady, setIsReady] = React.useState(false);
+  const fetchFonts = () => {
+    return Font.loadAsync({
+      // @ts-ignore
+      'ubuntu-bold': require('./assets/fonts/Ubuntu-Bold.ttf'),
+      // @ts-ignore
+      'ubuntu': require('./assets/fonts/Ubuntu-Regular.ttf')
+    });
+  }
+
+  if (!isReady) return <AppLoading onError={(err)=>console.log(err)} startAsync={fetchFonts} onFinish={()=>setIsReady(true)} />
+  return <AppNavigator />;
 }
